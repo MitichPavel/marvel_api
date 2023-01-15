c221099bf44ff307e5bd803134f5641f08727a05
Pavlo Mitich <mitich.pavel@gmail.com>
2023-01-15 21:31:16 +0100

    feat: added 404 page

---
+++ src/components/pages/Page404.js
@@ -0,0 +1,13 @@
+
+const Page404 = () => {
+  return (
+    <div style={{ 'height': '50vh', 'display': 'flex', 'text-align': 'center' }}>
+      <div style={{ 'margin': 'auto' }}>
+        <h1 style={{ 'font-size': '100px' }}>404</h1>
+        <p style={{ 'font-size': '40px' }}>Page not found</p>
+      </div>
+    </div>
+  )
+}
+
+export default Page404;
