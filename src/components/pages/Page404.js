0cc125fdec7e37ba22357a7294cba9c6d76d4e4f
Pavlo Mitich <mitich.pavel@gmail.com>
2023-01-16 19:47:03 +0100

    feat: add link to main page to Page404

--- src/components/pages/Page404.js
+++ src/components/pages/Page404.js
@@ -1,10 +1,14 @@
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
   return (
     <div style={{ 'height': '50vh', 'display': 'flex', 'text-align': 'center' }}>
       <div style={{ 'margin': 'auto' }}>
-        <h1 style={{ 'font-size': '100px' }}>404</h1>
+        <ErrorMessage />
+        <h1 style={{ 'margin-top': '20px', 'font-size': '100px' }}>404</h1>
         <p style={{ 'font-size': '40px' }}>Page not found</p>
+        <Link to="/" style={{ 'text-decoration': 'underline', 'margin-top': '20px', 'font-size': '20px' }}>Back to main page</Link>
       </div>
     </div>
   )
